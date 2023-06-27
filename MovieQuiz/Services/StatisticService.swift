3e448f536f967c16888560b689db19884dbc9a9e
TemerovaJaa <Snezhana.temerova@gmail.com>
2023-06-27 21:07:48 +0200

    V

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -5,6 +5,39 @@ protocol StatisticService {
     var totalAccuracy: Double { get }
     var gamesCount: Int { get }
     var bestGame: GameRecord { get }
+    func store(correct count: Int, total amount: Int)
 }
 final class StatisticServiceImplementation: StatisticService {
+    private let userDefaults = UserDefaults.standard
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    var totalAccuracy: Double = 0.0
+    
+    var gamesCount: Int = 0
+    
+    var bestGame: GameRecord {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+            let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                return .init(correct: 0, total: 0, date: Date())
+            }
+            
+            return record
+        }
+        
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    func store(correct count: Int, total amount: Int) {
+        <#code#>
+    }
+    
 }
